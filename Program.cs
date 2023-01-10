ef19d1e64e0d3a92ab4888cbba7bb1b99d4aa98d
Cyril Uwagwu <cyril@lemeritegroupe.com>
2023-01-10 04:24:47 +0100

    Initial

--- Program.cs
+++ Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string ans = string.Empty;
+do
+{
+    Console.Clear();
+    Console.Beep();
+    Console.WriteLine("______________________________________");
+    Console.WriteLine("Program to compute Fibonacc's numbers");
+    Console.WriteLine("______________________________________");
+    Console.WriteLine();
+
+    int a = 0, b = 1, c, i, numb;
+    Console.Write("Enter the number of elements to compute: ");
+    numb = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+
+    Console.Write(a + " " + b + " ");
+    for (i = 2; i < numb; ++i)
+    {
+        c = a + b;
+        Console.Write(c + " ");
+
+        a = b;
+        b = c;
+    }
+    Console.WriteLine();
+    Console.Beep();
+    Console.WriteLine();
+    Console.Write("Do you want to continue Yes(Y) or No(N)? ");
+    ans = Console.ReadLine();
+    Console.WriteLine();
+    
+} while (ans.ToLower() == "yes" || ans.ToLower() == "y");
+
+Console.Beep();
+Console.WriteLine("Thank you for using my app!");
+Console.WriteLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
